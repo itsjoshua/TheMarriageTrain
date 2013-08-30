20c7762294bfbcbef571bbd38b17932dd4ac09bb
Joshua Rajan <joshuarajan@gmail.com>
2013-08-29 19:04:20 -0500

    Initial setup of git on windows

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyWedding::Application.config.secret_key_base = '27c501edb056b6dbf436485a1fbcfb1d20bf33eadb89f954934d7b546f5cacebcc5af68645c9859e44184f50da9d9583ad0146edd0bddc0617c92b6a726dce3d'
